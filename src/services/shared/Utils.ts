b8daba5607fc242e49858b069941fcbbf51d4645
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-09-15 23:57:17 -0300

    project cleanup + add debug configuration for our lambda functions

--- src/services/shared/Utils.ts
+++ src/services/shared/Utils.ts
@@ -1,31 +1,6 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult, } from "aws-lambda";
-import { JsonError } from "./Validator";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { randomUUID } from "crypto";
 
-export function createRandomId(){
-    return randomUUID();
-}
-
-export function addCorsHeader(arg: APIGatewayProxyResult) {
-    if(!arg.headers) {
-        arg.headers = {}
-    }
-    arg.headers['Access-Control-Allow-Origin'] = '*';
-    arg.headers['Access-Control-Allow-Methods'] = '*';
-}
-
-export function parseJSON(arg: string){
-    try {
-        return JSON.parse(arg);
-    } catch (error) {
-        throw new JsonError(error.message)
-    }
-}
-
-export function hasAdminGroup(event: APIGatewayProxyEvent){
-    const groups = event.requestContext.authorizer?.claims['cognito:groups'];
-    if (groups) {
-        return (groups as string).includes('admins');
-    }
-    return false;
+export function createRandomId() {
+  return randomUUID();
 }
