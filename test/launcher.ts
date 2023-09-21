8cbf53dc1fa816d284aab76b240405d2a32f53e4
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-09-21 11:25:51 -0300

    implement async await syntax to the testRequest on launcher.ts

--- test/launcher.ts
+++ test/launcher.ts
@@ -1,14 +1,19 @@
 import { v4 } from "uuid";
 import { handler } from "../src/services/spaces/handler";
 
-handler(
-  {
-    httpMethod: "POST",
-    // queryStringParameters: { id: "278f11bf-b0c3-4bcc-94c2-64144353c1e4" },
-    body: JSON.stringify({
-      location: "Vancouver updated x2",
-      name: "Workspace in a mall",
-    }),
-  } as any,
-  {} as any
-).then((result) => console.log(result));
+const testRequest = async () => {
+  const result = await handler(
+    {
+      httpMethod: "POST",
+      // queryStringParameters: { id: "278f11bf-b0c3-4bcc-94c2-64144353c1e4" },
+      body: JSON.stringify({
+        location: "Vancouver updated x2",
+        name: "Workspace in a mall",
+      }),
+    } as any,
+    {} as any
+  );
+  console.log(result);
+};
+
+testRequest();
