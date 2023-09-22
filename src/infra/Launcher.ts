e5123bb2390e81829859d47525554ec2b3042373
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-09-21 21:14:44 -0300

    create userPool and userPoolId using AWS Cognito into a AuthStack

--- src/infra/Launcher.ts
+++ src/infra/Launcher.ts
@@ -2,12 +2,16 @@ import { App } from "aws-cdk-lib";
 import { ApiStack } from "./stacks/ApiStack";
 import { DataStack } from "./stacks/DataStack";
 import { LambdaStack } from "./stacks/LambdaStack";
+import { AuthStack } from "./stacks/AuthStack";
 
 const app = new App();
 const dataStack = new DataStack(app, "DataStack");
 const lambdaStack = new LambdaStack(app, "LambdaStack", {
   spacesTable: dataStack.spacesTable,
 });
+
+new AuthStack(app, "AuthStack");
+
 new ApiStack(app, "ApiStack", {
   spacesLambdaIntegration: lambdaStack.spacesLambda,
 });
---
+++ src/infra/stacks/AuthStack.ts
@@ -0,0 +1,40 @@
+import { CfnOutput, Stack, StackProps } from "aws-cdk-lib";
+import { UserPool, UserPoolClient } from "aws-cdk-lib/aws-cognito";
+import { Construct } from "constructs";
+
+export class AuthStack extends Stack {
+  private userPool: UserPool;
+  private userPoolClient: UserPoolClient;
+
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    this.createUserPool();
+    this.createUserPoolClient();
+  }
+
+  private createUserPool() {
+    this.userPool = new UserPool(this, "SpacesUserPool", {
+      selfSignUpEnabled: true,
+      signInAliases: { username: true, email: true },
+    });
+
+    new CfnOutput(this, "SpacesUserPoolId", {
+      value: this.userPool.userPoolId,
+    });
+  }
+  private createUserPoolClient() {
+    this.userPoolClient = this.userPool.addClient("SpacesUserPoolClient", {
+      authFlows: {
+        adminUserPassword: true,
+        custom: true,
+        userPassword: true,
+        userSrp: true,
+      },
+    });
+
+    new CfnOutput(this, "SpacesUserPoolClientId", {
+      value: this.userPoolClient.userPoolClientId,
+    });
+  }
+}
